14edd32aa6396739848ee4e8011f69ccb879a498
Jaylan <jaylanwienckowski@gmail.com>
2020-08-12 11:53:02 -0500

    setting up server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false
+});
+
+// Need to create routes
+
+app.listen(PORT,function(){ 
+    console.log(`App listening on Port ${PORT}`);
+});
