c0ce1e6831d5689f55666d3f953f86082114f200
Jaylan <jaylanwienckowski@gmail.com>
2020-08-15 14:48:00 -0500

    connecting to heroku

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(logger("dev"));
+
+const PORT = process.env.PORT || 3030;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI = MONGODB_URI || "mongodb://localhost/workout";
 
 mongoose.connect(MONGODB_URI,{  
     useNewUrlParser:true,
+    useUnifiedTopology: true,
     useFindAndModify:false
 });
 
+
+
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
+
 app.listen(PORT,function(){ 
     console.log(`App listening on Port ${PORT}`);
 });
