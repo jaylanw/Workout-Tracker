5392acb3bfbf3ab35cd7b659f5077e1b62f59e4b
Jaylan <jaylanwienckowski@gmail.com>
2020-08-15 22:37:35 -0500

    changing port number

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const logger = require("morgan");
 const app = express();
 app.use(logger("dev"));
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-var MONGODB_URI = MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 mongoose.connect(MONGODB_URI,{  
     useNewUrlParser:true,
